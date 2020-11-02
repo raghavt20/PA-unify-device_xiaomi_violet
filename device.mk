3565b4a926d02f512aab66db95df5a244ab7a12c
raghavt20 <raghavtandon2000@gmail.com>
2020-11-02 09:02:55 +0000

    violet: Update Radio Stack from davinci-user 11 RKQ1.200826.002 20.10.20 release-keys

--- device.mk
+++ device.mk
@@ -354,9 +354,10 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio.config@1.0 \
-    android.hardware.radio@1.4 \
-    android.hardware.secure_element@1.0 \
+    android.hardware.radio.config@1.1 \
+    android.hardware.radio@1.5 \
+    libavservices_minijail.vendor \
+    android.hardware.secure_element@1.1 \
     libjson \
     libprotobuf-cpp-full \
     librmnetctl \
