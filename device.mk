8fe78603df18699b61cf9aa3e947a6c687e5941e
raghavt20 <raghavtandon2000@gmail.com>
2020-11-04 09:17:13 +0000

    violet: Add hardware/google/pixel in soong namespace

--- device.mk
+++ device.mk
@@ -387,6 +387,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi \
+    hardware/google/pixel \
     vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
     vendor/qcom/opensource/commonsys/system/bt/conf
 
